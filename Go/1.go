d95190cfd1b71438dac921447be1d3826c954a78
cblanc <cablanchard@gmail.com>
2014-06-08 00:36:56 +0100

    More solutions

--- Go/1.go
+++ Go/1.go
@@ -6,14 +6,18 @@ import (
 
 func main() {
 	var multiples []int
+
 	for i := 1; i < 1000; i++ {
 		if i%5 == 0 || i%3 == 0 {
 			multiples = append(multiples, i)
 		}
 	}
+
 	result := 0
+
 	for _, e := range multiples {
 		result += e
 	}
+
 	fmt.Println(result)
 }
